475583369cbc12c657666908f5334a992b034a82
Hariharan Natarajan <hariharan.natarajan@snowfox.ai>
2021-03-22 22:08:40 +0200

    Add empty src directory

---
+++ src/main/fi/kesko/factory/KeskoFactory.java
@@ -0,0 +1,13 @@
+package fi.kesko.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeskoFactory {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeskoFactory.class, args);
+    }
+
+}
