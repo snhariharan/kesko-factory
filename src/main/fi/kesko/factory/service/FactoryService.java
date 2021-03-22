026ae39ee173ea44b63a09b709e1f86fd04f63be
Hariharan Natarajan <hariharan.natarajan@snowfox.ai>
2021-03-22 23:33:39 +0200

    create a simple code to implement the necessary functionality

--- src/main/fi/kesko/factory/service/FactoryService.java
+++ src/main/fi/kesko/factory/service/FactoryService.java
@@ -8,6 +8,28 @@ import org.springframework.stereotype.Component;
 public class FactoryService {
 
     public CheapestCombination getCheapestPriceCombo(CustomerRequest customerRequest) {
-        return null;
+        Integer boxCount = 0;
+        Integer packCount = 0;
+        Integer barCount = 0;
+        Float totalPrice = 0.0f;
+        Integer remaining = customerRequest.getCount();
+        Integer boxSize = customerRequest.getBoxSize();
+        Integer packSize = customerRequest.getPackSize();
+        do {
+            if (remaining > boxSize) {
+                boxCount = remaining / boxSize;
+                remaining = remaining % boxSize;
+                totalPrice += boxCount * customerRequest.getBoxPrice();
+            } else if (remaining > packSize) {
+                packCount = remaining / packSize;
+                remaining = remaining % packSize;
+                totalPrice += packCount * customerRequest.getPackPrice();
+            } else {
+                barCount = remaining;
+                remaining = 0;
+                totalPrice += barCount * customerRequest.getBarPrice();
+            }
+        } while (remaining > 0);
+        return new CheapestCombination(boxCount, packCount, barCount, totalPrice);
     }
 }
